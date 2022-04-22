65c0cec7c3b75060c015b75ebadcdfc61e25001a
MotaMiguel <miguel.mota@av.it.pt>
2022-04-22 11:22:52 +0100

    Changed example to show how kafka groups work. Necessary to be able to monitor times in topics already consumed by modules such as LCM

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,8 +1,16 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer("mep",bootstrap_servers='localhost:9093',
-                         auto_offset_reset="smallest",
-                         enable_auto_commit=True,
-                         group_id="test")
+import argparse
+def main(group):
+	consumer = KafkaConsumer("mep",bootstrap_servers='localhost:9093',
+	                         auto_offset_reset="smallest",
+	                         enable_auto_commit=True,
+	                         group_id=group)
 
-for msg in consumer:
-    print(f"Received message {msg.value.decode()} in topic {msg.topic} create with timestamp {msg.timestamp}")
+	for msg in consumer:
+	    print(f"Group {group} - Received message {msg.value.decode()} in topic {msg.topic} create with timestamp {msg.timestamp}")
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser("Consumer group example")
+	parser.add_argument("--group",type=str,required=False,default="test")
+	args = parser.parse_args()
+	main(args.group)
