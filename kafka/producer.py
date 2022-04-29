01e3f57b97792740bae66d9c8a54aa2bfafe76cf
MotaMiguel <miguel.mota@av.it.pt>
2022-04-30 00:47:50 +0100

    Files for kafka dump container
    Signed-off-by: MotaMiguel <miguel.mota@av.it.pt>

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,5 +2,5 @@ from kafka import KafkaProducer
 import time
 producer = KafkaProducer(bootstrap_servers='0.0.0.0:9093')
 for _ in range(100):
-    producer.send('mep', 'some_message_bytes'.encode())
+    producer.send('mep', 'some_message_bytes'.encode(),key=b"hello")
     producer.flush()
